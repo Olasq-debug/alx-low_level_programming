6fce500569472d420899141c1757256ddc0aafe1
BrianX <fatokunolalekan21@gmail.com>
2022-09-20 14:56:10 -0700

    FUctions

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,2 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * Main - Entry point
- *
- * Return: Always (0) (Success)
- */
-void main()
-{
-void reset_to_98(int *n);
 
-return (0);
-}
+void reset_to_98(int *n);
